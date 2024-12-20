88c21ac120e88620aee95b753c7763b602110994
Flowzy <flowzero@yandex.ru>
2024-12-20 08:46:23 +0300

    Adding new functionality: multiplication of two numbers

--- calculator.py
+++ calculator.py
@@ -1,12 +1,16 @@
 def add(x, y): 
     return x + y 
- 
+
 def subtract(x, y): 
     return x - y 
- 
+
+def multiply(x, y): 
+    return x * y 
+
 print("Выберите операцию:") 
 print("1. Сложение") 
 print("2. Вычитание") 
+print("3. Умножение") 
  
 while True: 
     choice = input("Введите номер операции (1/2/3/4): ") 
@@ -19,6 +23,8 @@ while True:
             print(f"{num1} + {num2} = {add(num1, num2)}") 
         elif choice == '2': 
             print(f"{num1} - {num2} = {subtract(num1, num2)}") 
+        elif choice == '3': 
+            print(f"{num1} * {num2} = {multiply(num1, num2)}") 
          
  
         # Спрашиваем пользователя, хочет ли он продолжить 
@@ -26,4 +32,4 @@ while True:
         if next_calculation.lower() != 'да': 
             break 
     else: 
-        print("Неверный ввод. Пожалуйста, выберите номер операции от 1 до 4.")
+        print("Неверный ввод. Пожалуйста, выберите номер операции от 1 до 4.") 
